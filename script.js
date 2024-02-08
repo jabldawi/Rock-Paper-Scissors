b4afd4b2db43658afa05d8183d0ae6cd497741e2
jabldawi <dawidjab988@gmail.com>
2024-02-08 18:38:47 +0100

    Add game logic for best of 1 and handle lower/upper case inputs

--- script.js
+++ script.js
@@ -2,12 +2,33 @@
 const rockPaperScissors = [`Rock`, `Paper`, `Scissors`];
 
 // Computer choosing random string within the array and storing it in variable computerSelection
-const randomComputerChoice = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+const computerSelection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
 
-const playerSelection = prompt(`Pick Rock, Paper or Scissors`);
+// Ask user to pick rock, paper or scissors and store it in playerSelection variable
+let playerSelection = prompt(`Pick Rock, Paper or Scissors`);
+playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+console.log(playerSelection);
 
-function playRound(computerSelection, playerSelection) {
-    
+function playGame(computerSelection, playerSelection) {
+    if (
+        (playerSelection === `Rock`) && (computerSelection === `Paper`) ||
+        (playerSelection === `Paper`) && (computerSelection === `Scissors`) ||
+        (playerSelection === `Scissors`) && (computerSelection === `Rock`)
+    ) {
+        return `You lose!`;
+    }
+    else if (
+        (playerSelection === `Rock`) && (computerSelection === `Scissors`) ||
+        (playerSelection === `Paper`) && (computerSelection === `Rock`) ||
+        (playerSelection === `Scissors`) && (computerSelection === `Paper`)
+    ) {
+        return `You win!`;
+    }
+    else {
+        return `It's a tie!`;
+    }
 }
+
+console.log(playGame(computerSelection, playerSelection));
 
 
