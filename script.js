0284e9d3dde38fa9a8c81b87466fa360d70639d4
jabldawi <dawidjab988@gmail.com>
2024-02-08 18:04:23 +0100

    add computer and user variables of Rock/Paper/Scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(`Hello World!`);
+// Initializing array with rock, paper scissors values
+const rockPaperScissors = [`Rock`, `Paper`, `Scissors`];
+
+// Computer choosing random string within the array and storing it in variable computerSelection
+const randomComputerChoice = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+
+const playerSelection = prompt(`Pick Rock, Paper or Scissors`);
+
+function playRound(computerSelection, playerSelection) {
+    
+}
+
+
